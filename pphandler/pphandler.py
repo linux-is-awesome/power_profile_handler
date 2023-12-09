c599bbf5cda2bf3526580a23c06e0a88bfbb3839
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2023-12-10 01:42:29 +0300

    Fix udev rule issue, using `systemd-ac-power` to check power source, minor code improvement

--- pphandler/pphandler.py
+++ pphandler/pphandler.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-import os, sys
-from gi.repository import Gio, GLib, GObject
+import os
+from gi.repository import Gio, GLib
 
 def trigger_handler(_, __, ___): os.system('/usr/bin/pphandler')
 
 proxy = None
 loop = None
+
+# run handler on the service startup as the udev rule is unable to read power profile on boot
+trigger_handler(None, None, None)
 
 loop = GLib.MainLoop()
 bus = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
@@ -16,4 +19,3 @@ proxy = Gio.DBusProxy.new_sync(bus, Gio.DBusProxyFlags.NONE, None,
                                'net.hadess.PowerProfiles', None)
 proxy.connect('g-properties-changed', trigger_handler)
 loop.run()
-
